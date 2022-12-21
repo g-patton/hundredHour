8a4a360b59abf9ccaca66f92d739fced45791566
Graham Patton <graham.m.patton@gmail.com>
2022-12-21 18:09:10 -0500

    updated to post.ejs to show user, added unlogged in partials to appropriate files

--- controllers/posts.js
+++ controllers/posts.js
@@ -33,8 +33,9 @@ module.exports = {
   getPost: async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
+      const user = await User.find({ user: req.user.id })
       const comments = await Comment.find({post: req.params.id}).sort({ createdAt: "desc" }).lean();
-      res.render("post.ejs", { post: post, user: req.user, comments: comments });
+      res.render("post.ejs", { post: post, user: req.user, comments: comments});
     } catch (err) {
       console.log(err);
     }
